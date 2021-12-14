86e81fbf8b06083bee251d582f53d6a7b84e9338
dev_abhishek <abhishek@litmusblox.io>
2021-12-14 19:53:13 +0530

    exceptions added

--- src/main/java/com/restapi/api/WeatherController.java
+++ src/main/java/com/restapi/api/WeatherController.java
@@ -1,6 +1,7 @@
 package com.restapi.api;
 
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
@@ -11,31 +12,46 @@ import org.springframework.web.client.RestTemplate;
 public class WeatherController {
 
     @RequestMapping(method = RequestMethod.GET, value = "/weather")
-    public @ResponseBody Object getWeatherByCity(@RequestParam String city) {
+    public @ResponseBody Object getWeatherByCity(@RequestParam String city) throws Exception {
 
         RestTemplate restTemplate = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
 
-        ResponseEntity<Object> response = restTemplate.
-                getForEntity("https://api.openweathermap.org/data/2.5/weather?q=" + city + "," +
-                                "&APPID=80730ceb55c51bebae397cfc46e03344"+"&units=metric",
-                        Object.class);
+        if (city.isEmpty())
+            throw new NullPointerException("City Name can not be null");
+        ResponseEntity<Object> response;
 
-        return response;
+        try{
+             response = restTemplate.
+                    getForEntity("https://api.openweathermap.org/data/2.5/weather?q=" + city + "," +
+                                    "&APPID=80730ceb55c51bebae397cfc46e03344"+"&units=metric",
+                            Object.class);
+
+            return response;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/weather/forecast")
     public @ResponseBody Object getForecastByCity(@RequestParam String city) {
 
         RestTemplate restTemplate = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
+        ResponseEntity<Object> response;
 
-        ResponseEntity<Object> response = restTemplate.
-                getForEntity("https://api.openweathermap.org/data/2.5/forecast?q=" + city + "," +
-                                "&APPID=" + Configurations.API_KEY,
-                        Object.class);
+        if (city.isEmpty())
+            throw new NullPointerException("City Name can not be null");
 
-        return response;
+        try{
+            response = restTemplate.
+                    getForEntity("https://api.openweathermap.org/data/2.5/forecast?q=" + city + "," +
+                                    "&APPID=" + Configurations.API_KEY,
+                            Object.class);
+            return response;
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return null;
     }
 
 }
